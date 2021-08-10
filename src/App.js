87f9232669150a6a6f9e61cee8332a85b601227b
ragavkumarv <ragavkumarv@gmail.com>
2021-08-11 03:04:20 +0530

    intial 4 image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import "./sass/main.scss";
-
 // Components
-import Header from "./components/Header";
+//
+import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import Banner from "./components/Banner";
+import Header from "./components/Header";
 import Loader from "./components/Loader";
+import "./sass/main.scss";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -16,21 +17,28 @@ function App() {
   }, [loading]);
 
   return (
-    <>
-      {loading ? (
-        <Loader setLoading={setLoading} />
-      ) : (
-        <>
-          <Header />
-          <Banner />
-          {!loading && (
-            <div className="transition-image final">
-              <img src={process.env.PUBLIC_URL + `/images/image-2.jpg`} />
-            </div>
-          )}
-        </>
-      )}
-    </>
+    <AnimateSharedLayout type="crossfade">
+      <AnimatePresence>
+        {loading ? (
+          <motion.div key="loader">
+            <Loader setLoading={setLoading} />
+          </motion.div>
+        ) : (
+          <>
+            <Header />
+            <Banner />
+            {!loading && (
+              <div className="transition-image final">
+                <motion.img
+                  layoutId="main-image-1"
+                  src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
+                />
+              </div>
+            )}
+          </>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
   );
 }
 
